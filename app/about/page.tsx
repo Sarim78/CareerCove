f6e4490cbaa1812ab7b8b85f04fd3978b9f2bf90
Sarim <74938278+Sarim78@users.noreply.github.com>
2025-10-29 00:28:08 -0400

    Test 1

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+
 import { useEffect, useRef } from "react"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
